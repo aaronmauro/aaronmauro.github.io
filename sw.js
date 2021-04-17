7ab230980c99b99736808efc399b1fbd3f142681
Aaron <amauro@brocku.ca>
2021-04-17 11:34:15 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-388e2d4706cc834fcd93.js"
+    "url": "webpack-runtime-c5c0a376ede8b3336598.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6f8f399cf42dccf9c28a117a2c921e13"
+    "revision": "81115d958a84a2820c3be1bafd425ae7"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
