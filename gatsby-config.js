2bd0f545133abce255f7fbcce748018532469db9
Aaron Mauro <aaronmauro@users.noreply.github.com>
2022-08-23 21:31:48 -0400

    updated app name

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Aaron Mauro - Faculty Profile`,
+        short_name: `Aaron Mauro`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
